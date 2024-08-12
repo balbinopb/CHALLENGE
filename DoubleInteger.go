c49639c51ac74288b099b80751701c86d89c1c0d
Balbino Pedro Baptista <149806035+balbinopb@users.noreply.github.com>
2024-08-12 23:54:23 +0700

    #day69

---
+++ DoubleInteger.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func DoubleInteger(i int) int {
+  return 2*i
+}
+func main(){
+
+fmt.Println(DoubleInteger(2))
+fmt.Println(DoubleInteger(4))
+fmt.Println(DoubleInteger(7))
+
+}
